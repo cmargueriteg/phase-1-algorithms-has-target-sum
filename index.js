a059396a08e75d7d0aaa81eea1f18020f8d2fd4a
Cindy Germain <cindymarguerite@gmail.com>
2022-04-17 02:43:37 -0600

    commit tree 697a4f389bc2358cf6bde1163595c777c2c6df46

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+ 
+  for( let i = 0; i < array.length-1; i++){
+    for( let j = i + 1; j < array.length; j++){
+      if( array[i] + array[j] === target){
+        return true 
+      } 
+          
+        
+        
+      }
+      
+    }
+    return false
+  }
+
+
 
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
-  Add your pseudocode here
+  iterate thru array w/ for loop
+  add each element 
+  compare to target 
+  move to next element if not matching
 */
 
 /*
@@ -17,6 +34,13 @@ function hasTargetSum(array, target) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
+  console.log("MY TEST BE TRUE")  
+console.log("=>", hasTargetSum([1,2,3,4], 5))
+
+console.log("MY TEST BE FALSE")
+console.log("=>", hasTargetSum([4], 4))
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
@@ -29,6 +53,22 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+
+//console.log("MY TEST BE TRUE")  
+//console.log("=>", hasTargetSum([10,6,45,2], 47));
+
+//console.log("MY TEST BE TRUE")  
+//console.log("=>", hasTargetSum([11,2,4,51], 15));
+
+//console.log("MY TEST BE TRUE")  
+//console.log("=>", hasTargetSum([100,33,66,5], 133));
+
+//console.log("MY TEST BE FALSE")  
+//console.log("=>", hasTargetSum([2,2,3,3], 4))
+
+//console.log("MY TEST BE FALSE")  
+//console.log("=>", hasTargetSum([4], 4));
 }
 
 module.exports = hasTargetSum;
